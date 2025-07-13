a4181cece97690e1da16928b2e92ff6d67b9f4e9
phucnds <42528099+phucnds@users.noreply.github.com>
2025-07-13 09:03:03 +0700

    - up date

--- remote/maps/map_05/index.js
+++ remote/maps/map_05/index.js
@@ -1,7 +1,7 @@
-System.register("chunks:///_virtual/map_05",[],(function(){return{execute:function(){}}}));
+System.register("bundle://map_05/_virtual/map_05",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
-  r('virtual:///prerequisite-imports/map_05', 'chunks:///_virtual/map_05'); 
+  r('virtual:///prerequisite-imports/map_05', 'bundle://map_05/_virtual/map_05'); 
 })(function(mid, cid) {
     System.register(mid, [cid], function (_export, _context) {
     return {
